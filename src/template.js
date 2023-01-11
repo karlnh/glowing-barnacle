4f5ffb7286647600835262622532d7437ccbcca3
karlnh <hubigkarl@gmail.com>
2023-01-11 00:04:04 -0600

    init html template

---
+++ src/template.js
@@ -0,0 +1,24 @@
+const template =
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generated Team Profile</title>
+</head>
+<body>
+    <header style="background: red">
+        <h1>My Team</h1>
+    </header>
+    <main>
+        <section id="card-holder" style="display: flex">
+
+        </section>
+    </main>
+</body>
+</html>
+`
+
+module.exports = template;

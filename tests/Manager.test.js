b545dc2fb8202222faa2fa77ed64fecda0f155ce
karlnh <hubigkarl@gmail.com>
2023-01-10 23:32:15 -0600

    modify ordering of parameters in Manager

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -5,14 +5,14 @@ const Manager = require("../lib/Manager");
 describe("Manager", () => {
     describe("Object initialization", () => {
         it("should return that instanceof Manager is true when a new object is created with the constructor", () => {
-            const obj = new Manager("Karl", 1337, 306, "karl@gmail.com");
+            const obj = new Manager("Karl", 1337, "karl@gmail.com", 306);
 
             expect(obj instanceof Manager).toEqual(true);
         });
     });
     describe("Methods", () => {
         it("should return 'Manager' with getRole()", () => {
-            const obj = new Manager("Karl", 1337, 306, "karl@gmail.com");
+            const obj = new Manager("Karl", 1337, "karl@gmail.com", 306);
             const result = "Manager";
 
             const test = obj.getRole();
@@ -22,7 +22,7 @@ describe("Manager", () => {
     })
     describe("Parameter type exception tests", () => {
         it("should throw if officeNumber is not a number or empty", () => {
-            const cb = () => new Manager("Karl", 123, true, "karl@gmail.com");
+            const cb = () => new Manager("Karl", 123, "karl@gmail.com", true);
             expect(cb).toThrow("Parameter 'officeNumber' is either not a number or is undefined.");
         });
     })

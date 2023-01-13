5a80ba9fd2b73e4fb2c3018912fadf593331aa28
karlnh <hubigkarl@gmail.com>
2023-01-13 01:01:05 -0600

    add template, fs, write to file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // Referenced the following page:
 // https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/hierarchical.js
@@ -72,6 +73,66 @@ function generateTeam() {
     // using the teamMembers object,
     // fill in HTML template
     console.log(teamMembers);
+    const template =
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generated Team Profile</title>
+    <style>
+body {
+    font-family: monospace;
 }
+
+header {
+    background: lightsalmon;
+    color: black;
+    border: 1px solid black;
+    text-align: center;
+    padding: 1rem;
+}
+
+#card-holder {
+    display: flex;
+    place-content: center;
+    border: 1px solid black;
+    margin-top: 1rem;
+    padding: 1rem;
+}
+
+.card {
+    display: flex;
+    border: 1px solid black;
+    margin: 1rem;
+    padding: 2rem;
+}
+
+ul {
+    list-style: none;
+    /* margin: 0; */
+    padding: 0;
+}
+    </style>
+
+</head>
+<body>
+    <header>
+        <h1>My Team</h1>
+    </header>
+    <main>
+        <section id="card-holder">
+        <p>${teamMembers}</p>
+        </section>
+    </main>
+</body>
+</html>
+`
+    fs.writeFile('./dist/index.html', template, (err) =>
+    err ? console.error(err) : console.log("successly wrote file :)"));
+}
+
 
 init();

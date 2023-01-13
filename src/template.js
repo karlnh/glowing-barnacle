95fcc76cff3de8edc48d2f7b091818686dbd307f
karlnh <hubigkarl@gmail.com>
2023-01-12 22:42:45 -0600

    add html generation template

--- src/template.js
+++ src/template.js
@@ -7,14 +7,49 @@ const template =
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Generated Team Profile</title>
+    <style>
+body {
+    font-family: monospace;
+}
+
+header {
+    background: lightsalmon;
+    color: black;
+    border: 1px solid black;
+    text-align: center;
+    padding: 1rem;
+}
+
+#card-holder {
+    display: flex;
+    place-content: center;
+    border: 1px solid black;
+    margin-top: 1rem;
+    padding: 1rem;
+}
+
+.card {
+    display: flex;
+    border: 1px solid black;
+    margin: 1rem;
+    padding: 2rem;
+}
+
+ul {
+    list-style: none;
+    /* margin: 0; */
+    padding: 0;
+}
+    </style>
+
 </head>
 <body>
-    <header style="background: red">
+    <header>
         <h1>My Team</h1>
     </header>
     <main>
-        <section id="card-holder" style="display: flex">
-
+        <section id="card-holder">
+        
         </section>
     </main>
 </body>
